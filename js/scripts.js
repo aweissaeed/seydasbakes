e40bb3a449557673f8a6c248a2c78e749b47474d
aweis saeed <aweissaeed@aweiss-MacBook-Pro.local>
2018-03-19 20:47:11 +0000

    adds parallax and carousel

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('.carousel').carousel();
+	$('.parallax').parallax();
+});
+$('.carousel.carousel-slider').carousel({fullWidth: true});
+$('.carousel').carousel('next', 3); // Move next n times.
+
+      
